6f69fc60e61bcd1fd18c7f6b1b39619122a8bfbd
Lluismi <lmorey@cifpfbmoll.eu>
2020-09-23 01:33:13 +0200

    feat: Creada clase NumerosRomanos

---
+++ src/main/java/edu/elsmancs/NumerosRomanos/RomansNumbers.java
@@ -0,0 +1,23 @@
+package edu.elsmancs.NumerosRomanos;
+
+public class RomansNumbers {
+	
+	public String number = null;
+	public int numeroDecimal = 0;
+	
+    public String getnumber() {
+        return number;
+    }
+
+    public void setnumber(String number) {
+        this.number = number;
+    }
+
+    public int getNumeroDecimal() {
+        return numeroDecimal;
+    }
+
+    public void setNumeroDecimal(int numeroDecimal) {
+        this.numeroDecimal = numeroDecimal;
+    }
+}
